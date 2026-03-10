9f2979b0814d9344f31c4bcb338f1aec151ef3b9
merins7 <merinsathish7@gmail.com>
2026-03-10 07:07:24 +0000

    Added event and changed to _safemint

--- blockchain/src/FashionAuth.sol
+++ blockchain/src/FashionAuth.sol
@@ -10,15 +10,18 @@ contract FashionAuth is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
     mapping(bytes32 => uint256) public productHashToTokenId;
 
+    event ProductMinted(uint256 tokenId, address owner, bytes32 productHash);
+
     constructor() ERC721("Item Authentication", "IAUTH") Ownable(msg.sender) {}
 
     function mint(address to, string memory metadataURI, bytes32 productHash) public onlyOwner returns (uint256) {
         _tokenIds++;
         uint256 newTokenId = _tokenIds;
         require(productHashToTokenId[productHash] == 0, "Product hash already registered");
-        _mint(to, newTokenId);
+        _safeMint(to, newTokenId);
         _setTokenURI(newTokenId, metadataURI);
         productHashToTokenId[productHash] = newTokenId;
+        emit ProductMinted(newTokenId, to, productHash);
         return newTokenId;
     }
 }
