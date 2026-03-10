3507ed4a6f386514980242be02b5cf7c1791b7f9
merins7 <merinsathish7@gmail.com>
2026-03-10 10:01:35 +0000

    Added indexed in event verables and already register checking moved to beginning of function

--- blockchain/src/FashionAuth.sol
+++ blockchain/src/FashionAuth.sol
@@ -10,14 +10,14 @@ contract FashionAuth is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
     mapping(bytes32 => uint256) public productHashToTokenId;
 
-    event ProductMinted(uint256 tokenId, address owner, bytes32 productHash);
+    event ProductMinted(uint256 indexed tokenId, address indexed owner, bytes32 indexed productHash);
 
     constructor() ERC721("Item Authentication", "IAUTH") Ownable(msg.sender) {}
 
     function mint(address to, string memory metadataURI, bytes32 productHash) public onlyOwner returns (uint256) {
+        require(productHashToTokenId[productHash] == 0, "Product hash already registered");
         _tokenIds++;
         uint256 newTokenId = _tokenIds;
-        require(productHashToTokenId[productHash] == 0, "Product hash already registered");
         _safeMint(to, newTokenId);
         _setTokenURI(newTokenId, metadataURI);
         productHashToTokenId[productHash] = newTokenId;
