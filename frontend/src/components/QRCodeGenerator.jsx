949e158feac0a46dcf16860aa0083c54eb26a1f5
merins7 <merinsathish7@gmail.com>
2026-06-11 07:45:13 +0000

    Fix QR URL template string

--- frontend/src/components/QRCodeGenerator.jsx
+++ frontend/src/components/QRCodeGenerator.jsx
@@ -1,12 +1,12 @@
 import {QRCodeCanvas} from "qrcode.react";
 
 function QRCodeGenerator({tokenID}){
-    const verifyURL = 'https://auth-trace.vercel.app/verify/${tokenID}';
+    const verifyURL = `https://auth-trace.vercel.app/verify/${tokenID}`;
     return(
         <div>
             <h3>Scan to Verify Product</h3>
             <QRCodeCanvas value={verifyURL} size={200}/>
-            {/* <p>{verifyURL}</p> */}
+             <p>{verifyURL}</p> 
         </div>
     )
 }
